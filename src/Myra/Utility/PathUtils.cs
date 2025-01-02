8e35068c5269d56ba23be501b6ffdf888bb8a591
Robbie Lodico <pandacoder@pm.me>
2025-01-01 23:02:55 -0300

    fix: don't crash on non-Windows filesystems

--- src/Myra/Utility/PathUtils.cs
+++ src/Myra/Utility/PathUtils.cs
@@ -11,9 +11,7 @@ namespace Myra.Utility
 			get
 			{
 				string codeBase = Assembly.GetExecutingAssembly().Location;
-				UriBuilder uri = new UriBuilder(codeBase);
-				string path = Uri.UnescapeDataString(uri.Path);
-				return Path.GetDirectoryName(path);
+				return Path.GetDirectoryName(codeBase);
 			}
 		}
 
